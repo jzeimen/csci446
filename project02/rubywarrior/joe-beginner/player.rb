987b3323f0e3653aa3d215332103045311ba0f8e
Joe Zeimen <jzeimen@gmail.com>
2012-01-21 20:42:06 -0700

    Finished level 6

--- project02/rubywarrior/joe-beginner/player.rb
+++ project02/rubywarrior/joe-beginner/player.rb
@@ -1,11 +1,28 @@
 class Player
 	def initialize
 		@health = 0
+		@hitwall = false
 	end
   def play_turn(warrior)
+  	puts warrior.feel
 
   	@damagetaken = (warrior.health < @health)
   	@health = warrior.health
+
+  	#Start by going backwards
+  	if (!@hitwall)
+		if(warrior.feel(:backward).empty?) 
+			warrior.walk!(:backward)
+			return
+		elsif ( warrior.feel(:backward).captive?)
+			warrior.rescue!(:backward)
+			return
+		elsif (warrior.feel(:backward).wall?)
+			@hitwall = true
+		end
+	end
+
+	#Then forwards
   	if(warrior.feel.empty?) 
   		if @damagetaken
   			warrior.walk!
@@ -15,7 +32,12 @@ class Player
 	 	end
 	elsif(warrior.feel.captive?)
 		warrior.rescue!
-	else
+	elsif(warrior.feel.enemy?)
+		if warrior.health < 10
+			warrior.walk!(:backward) 
+			@hitwall = false
+			return
+		end
 		warrior.attack!
 	end
   end
