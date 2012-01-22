7bf419f95a2bbf9bd3a64a11ec7510718b6a3cc3
Joe Zeimen <jzeimen@gmail.com>
2012-01-21 20:07:54 -0700

    Finished level 4

--- project02/rubywarrior/joe-beginner/player.rb
+++ project02/rubywarrior/joe-beginner/player.rb
@@ -1,5 +1,19 @@
 class Player
+	def initialize
+		@health = 0
+	end
   def play_turn(warrior)
-  	warrior.walk!
+  	@damagetaken = (warrior.health < @health)
+  	@health = warrior.health
+  	if(warrior.feel.empty?) 
+  		if @damagetaken
+  			warrior.walk!
+  		else
+  			warrior.rest! if warrior.health < 20
+	  		warrior.walk! if warrior.health == 20
+	 	end
+	else 
+		warrior.attack!
+	end
   end
 end
