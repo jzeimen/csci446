f4113bd752c717796c17fc3aa52a787392caa0c0
Joe Zeimen <jzeimen@gmail.com>
2012-02-06 14:40:14 -0700

    Finished 14.1 creating users, and unit tests for that functionality.

--- project03/depot/db/schema.rb
+++ project03/depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120206053332) do
+ActiveRecord::Schema.define(:version => 20120206210510) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(:version => 20120206053332) do
     t.string   "decimal"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
