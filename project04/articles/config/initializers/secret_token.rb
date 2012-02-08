71d5cff4b0bd8c94b6333da714972ca96ac4541d
Joe Zeimen <jzeimen@gmail.com>
2012-02-08 15:48:44 -0700

    rails new articles

---
+++ project04/articles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '00aa36d19248cb9c3bb523c9b8eb29ce7b63bfb6ef294c119b591579311d855e5916cf156edde49da03d1fae74dc30f2ce68b3a063f2edfb4e7e252ca41397d6'
