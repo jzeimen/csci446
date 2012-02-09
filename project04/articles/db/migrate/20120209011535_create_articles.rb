a2e6081b3e4eab7d31090befdf30e9639a1a707e
Joe Zeimen <jzeimen@gmail.com>
2012-02-08 18:15:55 -0700

    rails generate scaffold Article title:string body:text author:string edits:integer

---
+++ project04/articles/db/migrate/20120209011535_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.string :author
+      t.integer :edits
+
+      t.timestamps
+    end
+  end
+end
