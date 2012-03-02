4426d04f46c3be70b9309498322111633e3153b1
Joe Zeimen <jzeimen@gmail.com>
2012-03-01 22:31:34 -0700

    Updated readme, added author validation

--- project05/app/controllers/articles_controller.rb
+++ project05/app/controllers/articles_controller.rb
@@ -6,7 +6,6 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @articles }
--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
 	belongs_to :author
 	validates :title, :body, :author, presence: true
-
+	validates_associated :author
 	before_update  {|article| article.edits+=1} #increment the article count if we update the model
 
 
