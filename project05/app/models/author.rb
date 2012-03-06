50c39ab573e559bdc87e3ff63410f556fdc0d73d
Joe Zeimen <jzeimen@gmail.com>
2012-03-05 21:46:49 -0700

    added paperclipping to author for image

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,10 +1,10 @@
 class Author < ActiveRecord::Base
 	validates :name, presence: true
-	has_many :articles
+	has_many :articles, dependent: :destroy
 	validate :no_pat
 	def no_pat
 		errors.add(:name, "Author's name cannot include Pat") if name.downcase.include?("pat")
 	end
 	
-
+	has_attached_file :photo, styles: {small: "150x150>"}
 end
